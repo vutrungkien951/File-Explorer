af4b408318170d2d92fbebbbcb4923a1b0f86792
vutrungkien951 <vutrungkien951@gmail.com>
2021-12-02 15:42:24 +0700

    Update tree view and folder list listview

--- WindowExplorers/Form1.Designer.cs
+++ WindowExplorers/Form1.Designer.cs
@@ -29,6 +29,7 @@ namespace WindowExplorers
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
@@ -40,10 +41,16 @@ namespace WindowExplorers
             this.btBack = new System.Windows.Forms.Button();
             this.btFoward = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtPath = new System.Windows.Forms.TextBox();
             this.btOpen = new System.Windows.Forms.Button();
-            this.treeView1 = new System.Windows.Forms.TreeView();
-            this.listView1 = new System.Windows.Forms.ListView();
+            this.treeView = new System.Windows.Forms.TreeView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.listView = new System.Windows.Forms.ListView();
+            this.nameFile = new System.Windows.Forms.ColumnHeader();
+            this.Date = new System.Windows.Forms.ColumnHeader();
+            this.Type = new System.Windows.Forms.ColumnHeader();
+            this.Size = new System.Windows.Forms.ColumnHeader();
+            this.imageListSmallIcon = new System.Windows.Forms.ImageList(this.components);
             this.tbSearchBar = new System.Windows.Forms.TextBox();
             this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
@@ -63,7 +70,7 @@ namespace WindowExplorers
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
             this.toolStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
-            this.toolStrip1.Size = new System.Drawing.Size(800, 56);
+            this.toolStrip1.Size = new System.Drawing.Size(700, 42);
             this.toolStrip1.TabIndex = 0;
             this.toolStrip1.Text = "toolStrip1";
             // 
@@ -73,7 +80,7 @@ namespace WindowExplorers
             this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
             this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButton1.Name = "toolStripButton1";
-            this.toolStripButton1.Size = new System.Drawing.Size(109, 53);
+            this.toolStripButton1.Size = new System.Drawing.Size(91, 39);
             this.toolStripButton1.Text = "New Folder";
             // 
             // toolStripButton2
@@ -81,7 +88,7 @@ namespace WindowExplorers
             this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
             this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButton2.Name = "toolStripButton2";
-            this.toolStripButton2.Size = new System.Drawing.Size(55, 53);
+            this.toolStripButton2.Size = new System.Drawing.Size(50, 39);
             this.toolStripButton2.Text = "Cut";
             // 
             // toolStripButton3
@@ -89,7 +96,7 @@ namespace WindowExplorers
             this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
             this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButton3.Name = "toolStripButton3";
-            this.toolStripButton3.Size = new System.Drawing.Size(67, 53);
+            this.toolStripButton3.Size = new System.Drawing.Size(59, 39);
             this.toolStripButton3.Text = "Copy";
             // 
             // toolStripButton4
@@ -97,7 +104,7 @@ namespace WindowExplorers
             this.toolStripButton4.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton4.Image")));
             this.toolStripButton4.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButton4.Name = "toolStripButton4";
-            this.toolStripButton4.Size = new System.Drawing.Size(67, 53);
+            this.toolStripButton4.Size = new System.Drawing.Size(59, 39);
             this.toolStripButton4.Text = "Paste";
             // 
             // toolStripButton5
@@ -105,7 +112,7 @@ namespace WindowExplorers
             this.toolStripButton5.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton5.Image")));
             this.toolStripButton5.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButton5.Name = "toolStripButton5";
-            this.toolStripButton5.Size = new System.Drawing.Size(87, 53);
+            this.toolStripButton5.Size = new System.Drawing.Size(74, 39);
             this.toolStripButton5.Text = "Rename";
             // 
             // toolStripButton6
@@ -114,23 +121,25 @@ namespace WindowExplorers
             this.toolStripButton6.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton6.Image")));
             this.toolStripButton6.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButton6.Name = "toolStripButton6";
-            this.toolStripButton6.Size = new System.Drawing.Size(80, 53);
+            this.toolStripButton6.Size = new System.Drawing.Size(64, 39);
             this.toolStripButton6.Text = "Properties";
             // 
             // btBack
             // 
-            this.btBack.Location = new System.Drawing.Point(0, 59);
+            this.btBack.Location = new System.Drawing.Point(0, 44);
+            this.btBack.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btBack.Name = "btBack";
-            this.btBack.Size = new System.Drawing.Size(55, 29);
+            this.btBack.Size = new System.Drawing.Size(48, 22);
             this.btBack.TabIndex = 1;
             this.btBack.Text = "<<";
             this.btBack.UseVisualStyleBackColor = true;
             // 
             // btFoward
             // 
-            this.btFoward.Location = new System.Drawing.Point(61, 59);
+            this.btFoward.Location = new System.Drawing.Point(53, 44);
+            this.btFoward.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btFoward.Name = "btFoward";
-            this.btFoward.Size = new System.Drawing.Size(55, 29);
+            this.btFoward.Size = new System.Drawing.Size(48, 22);
             this.btFoward.TabIndex = 2;
             this.btFoward.Text = ">>";
             this.btFoward.UseVisualStyleBackColor = true;
@@ -138,81 +147,136 @@ namespace WindowExplorers
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(122, 63);
+            this.label1.Location = new System.Drawing.Point(107, 47);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 20);
+            this.label1.Size = new System.Drawing.Size(37, 15);
             this.label1.TabIndex = 3;
             this.label1.Text = "Path :";
             // 
-            // textBox1
+            // txtPath
             // 
-            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.txtPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.Location = new System.Drawing.Point(173, 59);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(466, 27);
-            this.textBox1.TabIndex = 4;
+            this.txtPath.Location = new System.Drawing.Point(151, 44);
+            this.txtPath.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(408, 23);
+            this.txtPath.TabIndex = 4;
             // 
             // btOpen
             // 
             this.btOpen.AllowDrop = true;
-            this.btOpen.Location = new System.Drawing.Point(645, 59);
+            this.btOpen.Location = new System.Drawing.Point(564, 44);
+            this.btOpen.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btOpen.Name = "btOpen";
-            this.btOpen.Size = new System.Drawing.Size(55, 29);
+            this.btOpen.Size = new System.Drawing.Size(48, 22);
             this.btOpen.TabIndex = 5;
             this.btOpen.Text = "Open";
             this.btOpen.UseVisualStyleBackColor = true;
             // 
-            // treeView1
+            // treeView
             // 
-            this.treeView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.treeView.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left)));
-            this.treeView1.Location = new System.Drawing.Point(0, 94);
-            this.treeView1.Name = "treeView1";
-            this.treeView1.Size = new System.Drawing.Size(223, 353);
-            this.treeView1.TabIndex = 6;
+            this.treeView.ImageIndex = 0;
+            this.treeView.ImageList = this.imageList1;
+            this.treeView.Location = new System.Drawing.Point(0, 70);
+            this.treeView.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.treeView.Name = "treeView";
+            this.treeView.SelectedImageIndex = 0;
+            this.treeView.Size = new System.Drawing.Size(196, 266);
+            this.treeView.TabIndex = 6;
+            this.treeView.BeforeCollapse += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeView_BeforeCollapse);
+            this.treeView.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeView_BeforeExpand);
+            this.treeView.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView_AfterSelect);
             // 
-            // listView1
+            // imageList1
             // 
-            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "images.jpg");
+            this.imageList1.Images.SetKeyName(1, "icons8_opened_folder.ico");
+            // 
+            // listView
+            // 
+            this.listView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(229, 94);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(571, 353);
-            this.listView1.TabIndex = 7;
-            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.nameFile,
+            this.Date,
+            this.Type,
+            this.Size});
+            this.listView.HideSelection = false;
+            this.listView.Location = new System.Drawing.Point(200, 70);
+            this.listView.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(500, 266);
+            this.listView.SmallImageList = this.imageListSmallIcon;
+            this.listView.TabIndex = 7;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.Details;
+            this.listView.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // nameFile
+            // 
+            this.nameFile.Text = "Name";
+            this.nameFile.Width = 200;
+            // 
+            // Date
+            // 
+            this.Date.Text = "Date modified";
+            this.Date.Width = 120;
+            // 
+            // Type
+            // 
+            this.Type.Text = "Type";
+            this.Type.Width = 100;
+            // 
+            // Size
+            // 
+            this.Size.Text = "Size";
+            // 
+            // imageListSmallIcon
+            // 
+            this.imageListSmallIcon.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageListSmallIcon.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageListSmallIcon.ImageStream")));
+            this.imageListSmallIcon.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageListSmallIcon.Images.SetKeyName(0, "File_Explorer_Icon.png");
             // 
             // tbSearchBar
             // 
             this.tbSearchBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.tbSearchBar.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.tbSearchBar.Location = new System.Drawing.Point(706, 59);
+            this.tbSearchBar.Location = new System.Drawing.Point(618, 44);
+            this.tbSearchBar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tbSearchBar.Name = "tbSearchBar";
-            this.tbSearchBar.Size = new System.Drawing.Size(94, 27);
+            this.tbSearchBar.Size = new System.Drawing.Size(83, 23);
             this.tbSearchBar.TabIndex = 8;
             this.tbSearchBar.Text = "Search";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.InactiveBorder;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(700, 338);
             this.Controls.Add(this.tbSearchBar);
-            this.Controls.Add(this.listView1);
-            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.listView);
+            this.Controls.Add(this.treeView);
             this.Controls.Add(this.btOpen);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtPath);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btFoward);
             this.Controls.Add(this.btBack);
             this.Controls.Add(this.toolStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "File Explorer";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.toolStrip1.ResumeLayout(false);
             this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
@@ -232,11 +296,17 @@ namespace WindowExplorers
         private System.Windows.Forms.Button btBack;
         private System.Windows.Forms.Button btFoward;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtPath;
         private System.Windows.Forms.Button btOpen;
-        private System.Windows.Forms.TreeView treeView1;
-        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TreeView treeView;
+        private System.Windows.Forms.ListView listView;
         private System.Windows.Forms.TextBox tbSearchBar;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ColumnHeader nameFile;
+        private System.Windows.Forms.ColumnHeader Date;
+        private System.Windows.Forms.ColumnHeader Type;
+        private System.Windows.Forms.ColumnHeader Size;
+        private System.Windows.Forms.ImageList imageListSmallIcon;
     }
 }
 
--- WindowExplorers/Form1.cs
+++ WindowExplorers/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace WindowExplorers
 {
@@ -17,5 +18,90 @@ namespace WindowExplorers
             InitializeComponent();
         }
 
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitFolder();
+        }
+
+        private void InitFolder()
+        {
+            string[] drives = Directory.GetLogicalDrives();
+            TreeNode node = null;
+            foreach (string drive in drives)
+            {
+                node = new TreeNode(drive);
+                treeView.Nodes.Add(node);
+                node.Nodes.Add("temp");
+            }
+        }
+
+        private void treeView_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            TreeNode node = e.Node;
+            node.Nodes.Clear();
+            node.ImageIndex = 1;
+            //
+            try
+            {
+                foreach (string dir in Directory.GetDirectories(node.FullPath))
+                {
+                    TreeNode newNode = node.Nodes.Add(Path.GetFileName(dir));
+                    newNode.Nodes.Add("Temp");
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void treeView_BeforeCollapse(object sender, TreeViewCancelEventArgs e)
+        {
+            e.Node.ImageIndex = 0;
+        }
+
+        private void treeView_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            
+            string path = e.Node.FullPath;
+            txtPath.Text = path;
+            try
+            {
+                if (Directory.Exists(e.Node.FullPath))
+                {
+                    listView.Items.Clear();
+                    string[] dirs = Directory.GetDirectories(path);
+
+
+                    foreach (string dir in dirs)
+                    {
+
+                        var item = new ListViewItem(Path.GetFileName(dir));
+                        var date = Directory.GetLastWriteTime(path);
+                        var strDate = date.ToShortDateString();
+                        item.SubItems.Add(strDate);
+                        var stringType = "File Folder";
+                        item.SubItems.Add(stringType);
+                        item.ImageIndex = 0;
+
+                        listView.Items.Add(item);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Folder can't open");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
     }
 }
